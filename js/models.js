18055b35b2f843c521a99216658627608f3e2853
Moers <shaaban.moe@gmail.com>
2023-03-08 00:56:24 +0100

    add list func

--- js/models.js
+++ js/models.js
@@ -67,11 +67,15 @@ class StoryList {
    * Returns the new Story instance
    */
 
-  async addStory( /* user, newStory */) {
-    // UNIMPLEMENTED: complete this function!
+  async addStory( user, {title, author, url} ) {
+    const story = {title, author, url}
     const config = user.loginToken;
-    const res = await axios.post(`${BASE_URL}/stories`, newStory, config);
+    const res = await axios.post(`${BASE_URL}/stories`, story, config);
     
+    const newStory = new Story(res.data.story);
+    this.stories.append(newStory);
+    user.ownStories.append(newStory)
+    return newStory;
   }
 }
 
